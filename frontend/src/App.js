451790585e0a264595c80045782280446fc13c5c
MartinaKostic <martina.kostic@gmail.com>
2024-03-26 15:29:33 +0100

    test app & environment

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import FamTree from "./components/SignIn/SignUp/FamilyTree/FamTree";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <FamTree/>
+    </div>
+  );
+}
+
+export default App;
